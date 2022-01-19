30f169890b8bbc16d81546ed06f736174bd2f3d1
juliagarciamaestre <juliagarciamaestre@gmail.com>
2022-01-18 21:55:17 -0800

    create new_board function

--- tictactoe.py
+++ tictactoe.py
@@ -1,20 +1,52 @@
 #Tic Tac Toe
 #By: Julia Garcia
 def main():
+    #Display initial board
     board_game= board()
     square_board(board_game)
+
+    #
     current_player = player("")
+
     position = player_position(current_player)
     print(position)
-    choice = player_choice(board_game,position,current_player)
-    print(choice)
-    square_board(board_game)
+    # choice = player_choice(board_game,position,current_player)
+    # print(choice)
+    # new_square_board(position,current_player)
+    choice = new_board(position,board_game,current_player)
+    current_board = square_board(choice)
+    print(current_board)
     
 
 
 def board():
     board = [1,2,3,4,5,6,7,8,9]
     return board
+
+def new_board(new_position,board,player):
+    if new_position == 0:
+        board[0] = player
+    if new_position == 1:
+        board[1] = player
+    if new_position == 2:
+        board[2] = player
+    if new_position == 3:
+        board[3] = player
+    if new_position == 4:
+        board[4] = player
+    if new_position == 5:
+        board[5] = player
+    if new_position == 6:
+        board[6] = player
+    if new_position == 7:
+        board[7] = player
+    if new_position == 8:
+        board[8] = player
+    if new_position == 9:
+        board[9] = player
+    return board
+    
+
 
 def square_board(board):
     print()
@@ -36,14 +68,16 @@ def player_position(player):
     position = int(position) - 1
     return position
 
-def player_choice(board,position,player):
-    choice = board[position]
-    current_player = player
-    choice = current_player
-    return choice
+# def player_choice(board,position,player):
+#     choice = board[position]
+#     current_player = player
+#     choice = current_player
+#     return choice
 
+# def new_square_board(position,player):
+#     board[position] = player
+#     square_board(board)
 
-# def new_square_board(position):
 
 
 if __name__ == "__main__":
